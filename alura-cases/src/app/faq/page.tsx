95f13a4b9fa77aa153328c5f98ee6f08f812530e
LucasRohr <lucasrc17@live.com>
2024-06-20 19:33:25 -0300

    refactor: :recycle: activate app router

--- alura-cases/src/app/faq/page.tsx
+++ alura-cases/src/app/faq/page.tsx
@@ -1,25 +1,23 @@
-"use client";
-
 import { Link } from "../../components";
 
 const FAQ_API_URL =
   "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
 
-export const getStaticProps = async () => {
+const getFaqData = async () => {
   const result = await fetch(FAQ_API_URL);
 
   if (result) {
     const parsedResponse = await result.json();
 
     return {
-      props: {
-        faqList: parsedResponse,
-      },
+      faqList: parsedResponse,
     };
   }
 };
 
-export default function FaqPage({ faqList }) {
+export default async function FaqPage() {
+  const faqData = await getFaqData();
+
   return (
     <div>
       <h1>Alura Cases FAQ</h1>
@@ -28,7 +26,7 @@ export default function FaqPage({ faqList }) {
       </Link>
 
       <ul>
-        {faqList.map(({ question, answer }, index) => (
+        {faqData?.faqList.map(({ question, answer }, index) => (
           <li key={index}>
             <article>
               <h2>{question}</h2>
