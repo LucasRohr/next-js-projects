1ce0743e1440d5a039ce6253d728f1301d4c3223
LucasRohr <lucasrc17@live.com>
2024-06-23 16:01:09 -0300

    feat: :sparkles: add project SSG build

---
+++ next-features/next.config.js
@@ -0,0 +1,6 @@
+// Next build config file for homolog/production deploys
+
+module.exports = {
+  trailingSlash: true, // Always have a slash at route end
+  output: "export", // export = Static Site Generation build option
+};
